8bdc03291f52612e189b4fb3ad410f992a92a71d
Georgy Gram <geogram@list.ru>
2018-05-03 15:56:01 +0300

    Created splashActivity

--- app/src/main/java/geogram/example/galleryforyou/ui/activities/MainActivity.java
+++ app/src/main/java/geogram/example/galleryforyou/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package geogram.example.galleryforyou;
+package geogram.example.galleryforyou.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import geogram.example.galleryforyou.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/geogram/example/galleryforyou/ui/activities/SplashActivity.java
@@ -0,0 +1,20 @@
+package geogram.example.galleryforyou.ui.activities;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import geogram.example.galleryforyou.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemClock.sleep(1500);//показываем превью 1,5 секунды
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);//запускаем основную активность
+        finish();////завершаем превью активити
+    }
+}
